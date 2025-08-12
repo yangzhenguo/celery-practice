63b23441159b19b4f3bf247c585883ec0f98ca5a
Sam <sam@micous.com>
2025-08-12 16:16:34 +0800

    fix

--- celery_tasks/math.py
+++ celery_tasks/math.py
@@ -8,7 +8,7 @@ logger = get_task_logger(__name__)
 @shared_task(bind=True)
 def add(self, x, y):
     logger.info(f"Task {self.request.id} Adding {x} and {y}")
-    time.sleep(random.randint(1, 10))
+    time.sleep(random.randint(1, 5))
     logger.info(f"Task {self.request.id} Added {x} and {y}")
     return x + y
 
@@ -16,6 +16,6 @@ def add(self, x, y):
 @shared_task(bind=True)
 def multiply(self, x, y):
     logger.info(f"Task {self.request.id} Multiplying {x} and {y}")
-    time.sleep(random.randint(1, 10))
+    time.sleep(random.randint(1, 5))
     logger.info(f"Task {self.request.id} Multiplied {x} and {y}")
     return x * y
